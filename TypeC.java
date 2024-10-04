8a9cf420226d8bc5be9022f567d8c2b59fd707cb
Coleg.2005 <cole.garrison@gwu.edu>
2024-10-04 12:09:58 -0400

    TypeC written, test cases acting weird

--- TypeC.java
+++ TypeC.java
@@ -1,5 +1,32 @@
 import java.util.*;
 
 public class TypeC extends Thing{
+        private Random rand = new Random();
+
+        private int timeSinceLast = 0;
+        private int spiral = 1;
+
+        public TypeC(){
+                super(65, 50, 'g');
+        }
+
+        public void maybeTurn() {    
+               
+                
+
+                timeSinceLast++;
+                if(timeSinceLast == spiral){
+                        timeSinceLast = 0;
+                        int i = rand.nextInt(3);
+                        if (i == 1) {
+                                rightTurn();
+                        } else if (i == 2){
+                                leftTurn();
+                        }
+                        spiral++;
+                }
+            }   
+
+
        
 }
